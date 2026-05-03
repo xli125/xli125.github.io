0b8cc69b4fe738f7f915e6b7c2c635930acf07e1
xli125 <xiaolingli125@gmail.com>
2026-05-03 18:08:01 +0000

    Deploying to gh-pages from @ xli125/xli125.github.io@bfa5600a89e785287d6af1e3bfaff4c568179cb2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,13 +16,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-projects",
-          title: "projects",
-          description: "A growing collection of your cool projects.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/projects/";
-          },
         },{id: "post-a-post-with-plotly-js",
         
           title: "a post with plotly.js",
